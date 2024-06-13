813893797876c3a4cf91738e932ca2f422326318
ThanhNhann <thanhnhan98qh@gmail.com>
2024-06-13 18:16:38 +0700

    add GetIbcTxFromTxResponse

--- cosmos/cosmos.go
+++ cosmos/cosmos.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"time"
 	"encoding/json"
+	"strconv"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
@@ -115,6 +116,47 @@ func SendIBCTransfer(
 	}
 
 	return &txResponse, nil
+}
+
+func GetIbcTxFromTxResponse(txResp types.TxResponse) (tx ibc.Tx, _ error) {
+	tx.Height = uint64(txResp.Height)
+	tx.TxHash = txResp.TxHash
+	// In cosmos, user is charged for entire gas requested, not the actual gas used.
+	tx.GasSpent = txResp.GasWanted
+
+	const evType = "send_packet"
+	events := txResp.Events
+
+	var (
+		seq, _           = AttributeValue(events, evType, "packet_sequence")
+		srcPort, _       = AttributeValue(events, evType, "packet_src_port")
+		srcChan, _       = AttributeValue(events, evType, "packet_src_channel")
+		dstPort, _       = AttributeValue(events, evType, "packet_dst_port")
+		dstChan, _       = AttributeValue(events, evType, "packet_dst_channel")
+		timeoutHeight, _ = AttributeValue(events, evType, "packet_timeout_height")
+		timeoutTs, _     = AttributeValue(events, evType, "packet_timeout_timestamp")
+		data, _          = AttributeValue(events, evType, "packet_data")
+	)
+	tx.Packet.SourcePort = srcPort
+	tx.Packet.SourceChannel = srcChan
+	tx.Packet.DestPort = dstPort
+	tx.Packet.DestChannel = dstChan
+	tx.Packet.TimeoutHeight = timeoutHeight
+	tx.Packet.Data = []byte(data)
+
+	seqNum, err := strconv.Atoi(seq)
+	if err != nil {
+		return tx, fmt.Errorf("invalid packet sequence from events %s: %w", seq, err)
+	}
+	tx.Packet.Sequence = uint64(seqNum)
+
+	timeoutNano, err := strconv.ParseUint(timeoutTs, 10, 64)
+	if err != nil {
+		return tx, fmt.Errorf("invalid packet timestamp timeout %s: %w", timeoutTs, err)
+	}
+	tx.Packet.TimeoutTimestamp = ibc.Nanoseconds(timeoutNano)
+
+	return tx, nil
 }
 
 func (c CosmosChain) Height(ctx context.Context) (uint64, error) {
