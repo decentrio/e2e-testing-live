ec7db4730918f3c8dae0d04ce398de2ec17096d0
anhductn2001 <ntd1312000@gmail.com>
2024-06-21 12:21:18 +0700

    getERC20Bal

--- cosmos/account.go
+++ cosmos/account.go
@@ -3,14 +3,21 @@ package cosmos
 import (
 	"bytes"
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/big"
 	"net/http"
-	"os/exec"
+	"regexp"
+	"strings"
 
 	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -80,23 +87,66 @@ func (user *User) GetFaucet(api string) {
 	fmt.Println("Response Body:", string(body))
 }
 
-func (user *User) GetERC20Balance(jsonrpc, erc20Contract string) error {
+func GetEvmAddressFromAnyFormatAddress(addrs ...string) (evmAddrs []common.Address, err error) {
+	for _, addr := range addrs {
+		normalizedAddr := strings.ToLower(addr)
 
-	command := []string{
-		"q", "balance", user.Address, erc20Contract, "--rpc", jsonrpc,
+		if regexp.MustCompile(`^(0x)?[a-f\d]{40}$`).MatchString(normalizedAddr) {
+			evmAddrs = append(evmAddrs, common.HexToAddress(normalizedAddr))
+		} else if regexp.MustCompile(`^(0x)?[a-f\d]{64}$`).MatchString(normalizedAddr) {
+			err = fmt.Errorf("ERR: invalid address format: %s", normalizedAddr)
+			return
+		} else { // bech32
+			spl := strings.Split(normalizedAddr, "1")
+			if len(spl) != 2 || len(spl[0]) < 1 || len(spl[1]) < 1 {
+				err = fmt.Errorf("ERR: invalid bech32 address: %s", normalizedAddr)
+				return
+			}
+
+			var bz []byte
+			bz, err = sdk.GetFromBech32(normalizedAddr, spl[0])
+			if err != nil {
+				err = fmt.Errorf("ERR: failed to decode bech32 address %s: %s", normalizedAddr, err)
+				return
+			}
+
+			if len(bz) != 20 {
+				err = fmt.Errorf("ERR: bech32 address %s has invalid length, must be 20 bytes, got %s %d bytes", normalizedAddr, hex.EncodeToString(bz), len(bz))
+				return
+			}
+
+			evmAddrs = append(evmAddrs, common.BytesToAddress(bz))
+		}
 	}
 
-	// Create the command
-	cmd := exec.Command("devd", command...)
-	fmt.Println(cmd)
-	// Run the command and get the output
-	output, err := cmd.Output()
+	return
+}
+
+func (user *User) GetERC20Balance(jsonrpc, erc20Contract string) (*big.Int, error) {
+	var height int64
+	contextHeight := big.NewInt(height)
+
+	evmAddrs, err := GetEvmAddressFromAnyFormatAddress(user.Address, erc20Contract)
+	fmt.Println(err)
+	accountAddr := evmAddrs[0]
+	contractAddr := evmAddrs[1]
+
+	fmt.Println("Account", accountAddr)
+
+	ethClient8545, err := ethclient.Dial(jsonrpc)
 	if err != nil {
-		fmt.Println("Error executing command:", err)
-		return err
+		fmt.Println("Failed to connect to EVM Json-RPC:", err)
+		return big.NewInt(0), err
+	}
+	bz, err := ethClient8545.CallContract(context.Background(), ethereum.CallMsg{
+		To:   &contractAddr,
+		Data: append([]byte{0x70, 0xa0, 0x82, 0x31}, common.BytesToHash(accountAddr.Bytes()).Bytes()...), // balanceOf(address)
+	}, contextHeight)
+	if err != nil {
+		return big.NewInt(0), err
 	}
 
-	// Print the output
-	fmt.Println(string(output))
-	return err
+	tokenBalance := new(big.Int).SetBytes(bz)
+
+	return tokenBalance, nil
 }
